ac30b0161a5602783a9bb8d10ec8bfea67d7c253
harsh-vardhan77 <vardhanharsh81020@gmail.com>
2026-06-15 18:38:30 +0530

    basic structure of C

---
+++ add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter number a\n");
+    scanf("%d", &a);
+    printf("Enter number b\n ");
+    scanf("%d", &b);
+
+    printf("Sum: %d\n", a + b);
+    return 0;
+}
