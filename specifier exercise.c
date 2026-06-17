77d34cedb08539d0207c5be5555b7f36cb94b5f5
harsh-vardhan77 <vardhanharsh81020@gmail.com>
2026-06-17 16:10:49 +0530

    specifier exercise

---
+++ specifier exercise.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a=5;
+float b=3.2;
+printf("the value of a is %d, and the value of b is %f\n",a,b);
+return 0;
+}
+//output
+//the value of a is 5, and the value of b is 3.200
