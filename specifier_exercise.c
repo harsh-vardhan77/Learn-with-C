0672da89f82e1bcc1f3426194d59b9e61deea455
harsh-vardhan77 <vardhanharsh81020@gmail.com>
2026-06-17 17:03:03 +0530

    adding constant usage

--- specifier_exercise.c
+++ specifier_exercise.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#define PI 3.14159
 int main()
 {
-    int a = 5;
+   { int a = 5;
     float b = 3.2;
     
     // Example 1: Basic format specifiers
@@ -9,8 +10,28 @@ int main()
     
     // Example 2: Formatted float output (8 width, 7 decimal places)
     printf("%8.7f\n", b);
-    
-    return 0;
+   }
+   // Output:
+// the value of a is 5, and the value of b is 3.200
+
+
+   //example of constants
+    {
+    const int c = 10;
+    const float d = 6.4;
+   // d=7.5; // This line will cause a compilation error because 'd' is a constant and cannot be modified.
+    printf("the value of c is %d, and the value of d is %f\n", c, d);
+    }
+    // Output:
+// the value of c is 10, and the value of d is 6.400
+{
+int a = 5;
+    float b = 3.2;
+    // PI=7.33; // This line will cause a compilation error because 'PI' is a macro constant and cannot be modified.
+    printf("%f",PI);
 }
 // Output:
-// the value of a is 5, and the value of b is 3.200
+// 3.141590
+    return 0;
+}
+
