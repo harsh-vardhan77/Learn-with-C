743f2291fa842a73bf5a1b2502ff293f5ff8af63
harsh-vardhan77 <vardhanharsh81020@gmail.com>
2026-06-17 16:45:43 +0530

    specifie formatr

---
+++ specifier_exercise.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+    float b = 3.2;
+    
+    // Example 1: Basic format specifiers
+    printf("the value of a is %d, and the value of b is %f\n", a, b);
+    
+    // Example 2: Formatted float output (8 width, 7 decimal places)
+    printf("Formatted: %8.7f\n", b);
+    
+    return 0;
+}
+// Output:
+// the value of a is 5, and the value of b is 3.200
